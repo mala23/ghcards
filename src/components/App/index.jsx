db3930637d2abf0905fff40c91dea660190f8d38
Colin Nico Schmid <mail@mala23.me>
2018-02-28 21:45:57 +0100

    feature/cards

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import Card from '../Card'
+import Cards from '../Cards'
 
 class App extends Component {
   render() {
     return (
-      <Card />
+      <Cards />
     )
   }
 }
