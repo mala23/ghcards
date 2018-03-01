4daf4c3b44ee0f4881c821abe7f822ab723b3d5d
Colin Nico Schmid <mail@mala23.me>
2018-03-01 21:42:58 +0100

    create event handler

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,9 +1,15 @@
 import React, {Component } from 'react'
 
 class Form extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Event: form submit")
+  }
+
   render() {
+
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input type="text" placeholder="Github username" />
         <button type="submit">Add card</button>
       </form>
