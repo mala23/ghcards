ba37697c6ace6eb33a37851b738b801e0a3eaf32
Colin Nico Schmid <mail@mala23.me>
2018-03-01 21:38:48 +0100

    use state object

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,12 +3,28 @@ import CardList from '../CardList'
 import { data } from '../CardList/'
 import Form from '../Form'
 
+
 class App extends Component {
+  state = {
+    cards: [
+      {
+        name: "Luca Nils Schmid",
+        avatar_url: "https://avatars1.githubusercontent.com/u/7280956?v=4",
+        company: "Vimcar",
+      },
+      {
+        name: "Fadie Hannona",
+        avatar_url: "https://avatars1.githubusercontent.com/u/7240284?v=4",
+        company: "Deloitte Digital",
+      }
+    ]
+  }
+
   render() {
     return (
       <article>
         <Form />
-        <CardList cards={data}/>
+        <CardList cards={this.state.cards}/>
       </article>
     )
   }
